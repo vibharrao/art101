7a45d1ad20d5561d75dc5d09733928f111621f70
vibharrao <89775420+vibharrao@users.noreply.github.com>
2023-10-23 16:29:30 -0700

    checking

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -2,20 +2,6 @@
 // Author: Your Name
 // Date:
 
-// Constants
+var name = "ford";
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+document.writeln(name);
